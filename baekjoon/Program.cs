1b0bf25c58c56ff00929326fa7af58eda1fe72bc
AoiTuna <bestspark123@gmail.com>
2026-03-31 23:12:15 +0900

    1712 problem

--- baekjoon/Program.cs
+++ baekjoon/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using baekjoon._1712;
+using baekjoon.Interface;
+
+IProblem problem = new _1712();
+problem.Solve();
